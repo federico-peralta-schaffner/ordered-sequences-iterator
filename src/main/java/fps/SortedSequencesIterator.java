f3742cdb1d3386c44632f7efbfe559a932581719
Federico Peralta Schaffner <federico.peralta@gmail.com>
2019-04-16 10:46:11 -0300

    Added comments

--- src/main/java/fps/SortedSequencesIterator.java
+++ src/main/java/fps/SortedSequencesIterator.java
@@ -18,18 +18,28 @@ public class SortedSequencesIterator {
 
     public static <T extends Comparable<? super T>> Iterator<T> flatSort(List<Iterable<T>> input) {
 
+        // Preconditions check
         Objects.requireNonNull(input, "input cannot be null");
 
+        // Border case: empty input
         if (input.isEmpty()) return Collections.emptyIterator();
 
+        // Preparation:
+        //   - Blow up if there's some null iterable
+        //   - Convert iterables to iterators
+        //   - Remove iterators without elements
         List<Iterator<T>> iterators = input.stream()
                 .map(it -> Objects.requireNonNull(it, "input sequences cannot be null"))
                 .map(Iterable::iterator)
                 .filter(Iterator::hasNext)
                 .collect(Collectors.toList());
 
+        // Check border case again: empty iterators list
         if (iterators.isEmpty()) return Collections.emptyIterator();
 
+        // Group iterators list by their first element
+        // (We need a multimap because there can be
+        // duplicated elements among different iterators)
         TreeMap<T, List<Iterator<T>>> nextPerSequence = iterators.stream()
                 .collect(Collectors.groupingBy(
                         Iterator::next,
@@ -38,11 +48,11 @@ public class SortedSequencesIterator {
                                 Function.identity(),
                                 Collectors.toCollection(ArrayList::new))));
 
-        class Box {
-            Iterator<T> it;
-        }
+        // Helper Box class
+        class Box { Iterator<T> it; }
         Box box = new Box();
 
+        // Iterator to be returned
         return new Iterator<>() {
 
             @Override
@@ -52,17 +62,20 @@ public class SortedSequencesIterator {
 
             @Override
             public T next() {
-                // Get min element
+                // Get min element from iterators map
                 T min = nextPerSequence.firstKey();
 
                 if (min == null) throw new NoSuchElementException();
 
+                // Remove min element's entry and keep a reference to its iterator
                 nextPerSequence.computeIfPresent(min, (k, its) -> {
                     box.it = its.remove(0);
                     return its.isEmpty() ? null : its;
                 });
 
                 Iterator<T> sequenceIterator = box.it;
+
+                // Add next element from min element's iterator
                 if (sequenceIterator.hasNext()) {
                     nextPerSequence.computeIfAbsent(sequenceIterator.next(), k -> new ArrayList<>())
                             .add(sequenceIterator);
